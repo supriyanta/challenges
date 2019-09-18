73c9d2ec4d41c60661e2f6f593ffd6e1368719da
supriyanta <supriyanta34@gmail.com>
2019-09-18 15:13:30 +0530

    Day 1 initial commit

---
+++ florin_top/day_001/main.js
@@ -0,0 +1,44 @@
+const URL = `https://www.themealdb.com/api/json/v1/1/random.php`;
+
+const btn = document.querySelector("button");
+const ingredients = document.querySelector("#ingred-list");
+
+btn.addEventListener("click", async function getAMeal() {
+	fetch(URL)
+		.then(data => data.json())
+		.then(data => {
+			console.log(data);
+			let meal = data.meals[0];
+			console.log(meal);
+			paintUi(meal);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+});
+
+function makeIngredientItem(itemName, measure) {
+	let item = document.createElement("li");
+	item.innerText = `${itemName} -- ${measure}`;
+	ingredients.appendChild(item);
+}
+
+function paintUi(meal) {
+	document.querySelector("#recipe-name").textContent = meal.strMeal;
+	document.querySelector("#recipe-desc").textContent = meal.strInstructions;
+	document.querySelector("img").src = meal.strMealThumb;
+	document.querySelector("#category").textContent = meal.strCategory;
+	document.querySelector("#area").textContent = meal.strArea;
+	document.querySelector("#tags").textContent = !!meal.strTags
+		? meal.strTags.split(",").join(", ")
+		: "None";
+
+	let i = 1;
+	while (true) {
+		let attr = `strIngredient${i}`;
+		if (!meal[attr]) break;
+		let measure = `strMeasure${i}`;
+		makeIngredientItem(meal[attr], meal[measure]);
+		++i;
+	}
+}
